41c1239dff44a13a85dbbff8ac994c7aac39314c
KenNgyn10 <b00148233@mytudublin.ie>
2024-12-11 23:01:07 +0000

    v5
    added repo links

---
+++ FinalJoyKen1/Assets/Scripts/link.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class link : MonoBehaviour
+{
+  
+  
+public void OpenKlink(){            //Ken's Link
+
+    Application.OpenURL("https://github.com/KenNgyn10?tab=repositories");
+}
+
+public void OpenJlink(){        //Joy's Link
+
+    Application.OpenURL("https://github.com/B00153218/final/commits?author=B00153218");
+}
+
+
+}
